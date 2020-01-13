2ad16daba4697aaf3083c20f595ce71e2b0a8f1e
Marcos Garcia <dimonibdnsaba13@gmail.com>
2020-01-13 23:20:51 +0100

    registro actualizado

--- app/src/main/java/es/gracia/marcos/proyectom7/RegistroActivity.java
+++ app/src/main/java/es/gracia/marcos/proyectom7/RegistroActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import java.util.regex.Matcher;
@@ -15,7 +16,8 @@ import java.util.regex.Pattern;
 
 public class RegistroActivity extends AppCompatActivity {
 
-    EditText campoCorreo, campoUsuario, campoContraseña;
+    EditText campoCorreo, campoUsuario, campoContraseña, campoTelefono;
+    RadioButton rb_anorexia, rb_bulimia, rb_sobrepeso, rb_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +27,45 @@ public class RegistroActivity extends AppCompatActivity {
         campoCorreo = (EditText) findViewById(R.id.et_correoRegistro);
         campoUsuario = (EditText) findViewById(R.id.et_usuarioRegistro);
         campoContraseña = (EditText) findViewById(R.id.et_contraseñaRegistro);
+        campoTelefono = (EditText) findViewById(R.id.et_telefonoRegistro);
+
+        rb_anorexia = (RadioButton) findViewById(R.id.rb_anorexia);
+        rb_bulimia = (RadioButton) findViewById(R.id.rb_bulimia);
+        rb_sobrepeso = (RadioButton) findViewById(R.id.rb_sobrepeso);
+        rb_no = (RadioButton) findViewById(R.id.rb_no);
     }
 
     public void registrarUsuario(View view) {
         String correo = campoCorreo.getText().toString();
         String usuario = campoUsuario.getText().toString();
         String contraseña = campoContraseña.getText().toString();
+        String telefono = campoTelefono.getText().toString();
+        String enfermedad;
+
+        if (rb_anorexia.isChecked() == true) {
+            enfermedad = "Anorexia";
+        } else if (rb_bulimia.isChecked() == true) {
+            enfermedad = "Bulimia";
+        } else if (rb_sobrepeso.isChecked() == true) {
+            enfermedad = "Sobrepeso";
+        } else {
+            enfermedad = "No";
+        }
+
 
         //Comprobamos que el todos los campos tengan texto
-        if (!correo.equals("") && !usuario.equals("") && !contraseña.equals("")) {
+        if (!correo.equals("") && !usuario.equals("") && !contraseña.equals("") && !telefono.equals("")) {
             Pattern pattern = Pattern.compile("([a-z0-9]+(\\.?[a-z0-9])*)+@(([a-z]+)\\.([a-z]+))+");
             Matcher mather = pattern.matcher(correo);
 
             //Comprobamos que el mail sea correcto
-            if (mather.find()) {
+            if (mather.find() || telefono.length() > 9) {
                 //Cargamos los datos en el SharedPreferences de la Main Activity
                 MainActivity.editor.putString("mail", correo);
                 MainActivity.editor.putString("user", usuario);
                 MainActivity.editor.putString("pass", contraseña);
+                MainActivity.editor.putString("tel", telefono);
+                MainActivity.editor.putString("enf", enfermedad);
                 MainActivity.editor.commit();
 
                 //Mostramos un toast y cargamos la MainActivity
@@ -51,7 +74,7 @@ public class RegistroActivity extends AppCompatActivity {
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
             } else {
-                Toast toast = Toast.makeText(getApplicationContext(), "El correo no es correcto", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "El correo/telefono no es correcto", Toast.LENGTH_SHORT);
                 toast.show();
             }
         } else {
