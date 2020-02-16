0acd67114b9cc1b55522e8f7d02961f61316a7b2
Marcos Garcia <dimonibdnsaba13@gmail.com>
2020-02-16 21:31:57 +0100

    bar chart va

--- app/src/main/java/es/gracia/marcos/proyectom7/ui/progreso/progresoFragment.java
+++ app/src/main/java/es/gracia/marcos/proyectom7/ui/progreso/progresoFragment.java
@@ -9,12 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -29,13 +33,29 @@ public class progresoFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_progreso, container, false);
         barChart = root.findViewById(R.id.prograsoChart);
         ArrayList<BarEntry> barEntries = new ArrayList();
-        barEntries.add(new BarEntry(0, 5));
-        barEntries.add(new BarEntry(1, 3));
-        barEntries.add(new BarEntry(2, 4));
-        barEntries.add(new BarEntry(3, 3));
+        barEntries.add(new BarEntry(0, 23.4f));
+        barEntries.add(new BarEntry(1, 23.4f));
+        barEntries.add(new BarEntry(2, 23.4f));
+        barEntries.add(new BarEntry(3, 23.4f));
+        barEntries.add(new BarEntry(4, 23.4f));
+        barEntries.add(new BarEntry(5, 23.4f));
+        barEntries.add(new BarEntry(6, 23.4f));
+        barEntries.add(new BarEntry(7, 23.4f));
+        barEntries.add(new BarEntry(8, 23.4f));
+        barEntries.add(new BarEntry(9, 23.4f));
+        barEntries.add(new BarEntry(10, 23.4f));
         BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
         ArrayList<String> theDates = new ArrayList();
+        theDates.add("Enero");
+        theDates.add("Febrero");
+        theDates.add("Marzo");
+        theDates.add("Abril");
+        theDates.add("Enero");
+        theDates.add("Febrero");
+        theDates.add("Marzo");
+        theDates.add("Abril");
         theDates.add("Enero");
         theDates.add("Febrero");
         theDates.add("Marzo");
@@ -44,6 +64,8 @@ public class progresoFragment extends Fragment {
         BarData theData = new BarData(barDataSet);
         barChart.setData(theData);
         barChart.setScaleEnabled(false);
+        barChart.animateY(2000);
+        barChart.zoom(2f,1f,1f,1f);
 
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(theDates));
@@ -51,7 +73,17 @@ public class progresoFragment extends Fragment {
         xAxis.setDrawGridLines(false);
         xAxis.setDrawAxisLine(false);
         xAxis.setGranularity(1f);
-        barChart.animateY(2000);
+
+        YAxis yAxis = barChart.getAxisLeft();
+        yAxis.setAxisMaximum(35f);
+        yAxis.setAxisMinimum(12f);
+
+        YAxis rightAxis = barChart.getAxisRight();
+        rightAxis.setEnabled(false);
+        Legend legend = barChart.getLegend();
+        legend.setEnabled(false);
+        Description description = barChart.getDescription();
+        description.setEnabled(false);
 
         return root;
     }
