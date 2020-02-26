37f4c71fcea774631dd02508e0af080baac713ed
Marcos Garcia <dimonibdnsaba13@gmail.com>
2020-02-26 17:28:23 +0100

    cambios esteticos y en configuraion

--- app/src/main/java/es/gracia/marcos/proyectom7/ui/configuracion/ConfiguracionFragment.java
+++ app/src/main/java/es/gracia/marcos/proyectom7/ui/configuracion/ConfiguracionFragment.java
@@ -1,16 +1,25 @@
 package es.gracia.marcos.proyectom7.ui.configuracion;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.Locale;
+
+import es.gracia.marcos.proyectom7.MainActivity;
 import es.gracia.marcos.proyectom7.R;
 
-public class ConfiguracionFragment extends Fragment {
+public class ConfiguracionFragment extends Fragment{
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
