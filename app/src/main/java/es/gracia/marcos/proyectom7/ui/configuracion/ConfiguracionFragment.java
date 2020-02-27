dd64f1dd0a675f8ce0759c4f5dabbb78cddb2cfa
Marcos Garcia <dimonibdnsaba13@gmail.com>
2020-02-27 20:39:40 +0100

    configuracion ya cambia datos personales, falta cambiar idioma y cerrar sesion

--- app/src/main/java/es/gracia/marcos/proyectom7/ui/configuracion/ConfiguracionFragment.java
+++ app/src/main/java/es/gracia/marcos/proyectom7/ui/configuracion/ConfiguracionFragment.java
@@ -1,32 +1,153 @@
 package es.gracia.marcos.proyectom7.ui.configuracion;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Locale;
 
+import es.gracia.marcos.proyectom7.CajaNavegacionActivity;
 import es.gracia.marcos.proyectom7.MainActivity;
 import es.gracia.marcos.proyectom7.R;
 
+import static java.lang.Integer.parseInt;
+
 public class ConfiguracionFragment extends Fragment{
     private Button idioma;
+    private Button guardar;
+    private DatabaseReference mDatabase;
+    private TextView correo;
+    private EditText nombre;
+    private RadioButton rb_anorexia, rb_bulimia, rb_sobrepeso, rb_no;
     View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_configuracion, container, false);
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users/" + CajaNavegacionActivity.getUser().getUid());
+        correo = root.findViewById(R.id.configuracionCorreo);
+        nombre = root.findViewById(R.id.configuracionNombre);
+        rb_anorexia = (RadioButton) root.findViewById(R.id.configuracion_rb_anorexia);
+        rb_bulimia = (RadioButton) root.findViewById(R.id.configuracion_rb_bulimia);
+        rb_sobrepeso = (RadioButton) root.findViewById(R.id.configuracion_rb_sobrepeso);
+        rb_no = (RadioButton) root.findViewById(R.id.configuracion_rb_no);
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                correo.setText(dataSnapshot.child("correo").getValue().toString());
+                nombre.setText(dataSnapshot.child("nombre").getValue().toString());
+                switch (parseInt(dataSnapshot.child("trastorno").getValue().toString())) {
+                    case 0:
+                        rb_no.setChecked(true);
+                        break;
+                    case 1:
+                        rb_anorexia.setChecked(true);
+                        break;
+                    case 2:
+                        rb_sobrepeso.setChecked(true);
+                        break;
+                    case 3:
+                        rb_bulimia.setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                return;
+            }
+        });
+
+        guardar = root.findViewById(R.id.btnGuardarDatos);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("")
+                        .setMessage("Seguro que quieres cambiar los datos?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        mDatabase.child("nombre").setValue(nombre.getText().toString());
+                                        String trastorno;
+                                        if (rb_anorexia.isChecked() == true) {
+                                            trastorno = "1";
+                                        } else if (rb_bulimia.isChecked() == true) {
+                                            trastorno = "2";
+                                        } else if (rb_sobrepeso.isChecked() == true) {
+                                            trastorno = "3";
+                                        } else {
+                                            trastorno = "0";
+                                        }
+                                        mDatabase.child("trastorno").setValue(trastorno);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        return;
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        correo.setText(dataSnapshot.child("correo").getValue().toString());
+                                        nombre.setText(dataSnapshot.child("nombre").getValue().toString());
+                                        switch (parseInt(dataSnapshot.child("trastorno").getValue().toString())) {
+                                            case 0:
+                                                rb_no.setChecked(true);
+                                                break;
+                                            case 1:
+                                                rb_anorexia.setChecked(true);
+                                                break;
+                                            case 2:
+                                                rb_sobrepeso.setChecked(true);
+                                                break;
+                                            case 3:
+                                                rb_bulimia.setChecked(true);
+                                                break;
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        return;
+                                    }
+                                });
+                            }
+                        })
+                        .show();
+            }
+        });
         idioma = root.findViewById(R.id.botonIdioma);
         idioma.setOnClickListener(new View.OnClickListener() {
             @Override
